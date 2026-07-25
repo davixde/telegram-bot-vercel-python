c37ac2fc1412dfd54a96ec462d4a984c2cdea877
davixde <davide.petterlin@icloud.com>
2026-07-25 12:11:42 +0200

    to-title-case fix

--- telegram_bot/static/example/webapp.js
+++ telegram_bot/static/example/webapp.js
@@ -78,7 +78,7 @@ function updateMapLanguage(lang) {
         if (typeof expr === 'string') {
             if (expr === '{name}' || expr === 'name') {
                 const coalesced = ['coalesce', ['get', `name:${targetLang}`], ['get', `name_${targetLang}`], ['get', 'name']];
-                return ['to-title-case', coalesced];
+                return ['concat', ['upcase', ['substr', coalesced, 0, 1]], ['substr', coalesced, 1]];
             }
             return expr;
         }
@@ -95,7 +95,7 @@ function updateMapLanguage(lang) {
                 expr
             ];
 
-            return ['to-title-case', coalesced];
+            return ['concat', ['upcase', ['substr', coalesced, 0, 1]], ['substr', coalesced, 1]];
         }
 
         return expr.map(child => injectLanguage(child, targetLang));
